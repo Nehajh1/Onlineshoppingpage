25298f7a36eb244af425add1e6557138b80e43c8
Neha <nehajha8413@gmail.com>
2024-11-12 14:22:31 +0530

    Create footer.js

---
+++ footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const Footer = () => { 
+return (
+<div className='bg-dark text-white p-3 d-flex justify-content-center '>
+<footer>
+&copy; Copyright 2018, SatyaCorporation
+</footer>
+</div>
+) 
+}
+export default Footer
